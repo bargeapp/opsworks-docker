395076007fb38844960e9c3e0786ba9cc4190b0d
Noah Blumenthal <noah@hackerhasid.com>
2015-01-10 20:12:46 -0500

    let there be light

---
+++ recipes/install.rb
@@ -0,0 +1,16 @@
+# taken straight from http://blogs.aws.amazon.com/application-management/post/Tx2FPK7NJS5AQC5/Running-Docker-on-AWS-OpsWorks
+
+case node[:platform]
+when "ubuntu","debian"
+  package "docker.io" do
+    action :install
+  end
+when 'centos','redhat','fedora','amazon'
+  package "docker" do
+    action :install
+  end
+end
+
+service "docker" do
+  action :start
+end
